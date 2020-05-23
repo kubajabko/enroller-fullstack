3b0193ce6553ecf7e2df4f3a9fbdde6b276c8530
Jakub Chochol <kubachochol94@gmail.com>
2020-05-23 13:15:17 +0200

    meetings go to db, but are not listed from db

--- src/main/frontend/src/meetings/MeetingsPage.vue
+++ src/main/frontend/src/meetings/MeetingsPage.vue
@@ -1,20 +1,20 @@
 <template>
-  <div>
-    <new-meeting-form @added="addNewMeeting($event)"></new-meeting-form>
+    <div>
+        <new-meeting-form @added="addNewMeeting($event)"></new-meeting-form>
 
-    <span v-if="meetings.length == 0">
+        <span v-if="meetings.length == 0">
                Brak zaplanowanych spotkań.
            </span>
-    <h3 v-else>
-      Zaplanowane zajęcia ({{ meetings.length }})
-    </h3>
+        <h3 v-else>
+            Zaplanowane zajęcia ({{ meetings.length }})
+        </h3>
 
-    <meetings-list :meetings="meetings"
-                   :username="username"
-                   @attend="addMeetingParticipant($event)"
-                   @unattend="removeMeetingParticipant($event)"
-                   @delete="deleteMeeting($event)"></meetings-list>
-  </div>
+        <meetings-list :meetings="meetings"
+                       :username="username"
+                       @attend="addMeetingParticipant($event)"
+                       @unattend="removeMeetingParticipant($event)"
+                       @delete="deleteMeeting($event)"></meetings-list>
+    </div>
 </template>
 
 <script>
@@ -31,7 +31,7 @@
         },
         methods: {
             addNewMeeting(meeting) {
-                this.meetings.push(meeting);
+                this.$http.post('meetings', meeting);
             },
             addMeetingParticipant(meeting) {
                 meeting.participants.push(this.username);
@@ -42,6 +42,12 @@
             deleteMeeting(meeting) {
                 this.meetings.splice(this.meetings.indexOf(meeting), 1);
             }
+        },
+        mounted() {
+            fetch(this.$http.get('meetings'))
+                .then((data) => {
+                    this.meetings = data;
+                })
         }
     }
 </script>
--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
 import org.hibernate.Query;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -20,6 +21,15 @@ public class MeetingService {
         Query query = connector.getSession().createQuery(hql);
         return query.list();
     }
+    public Meeting findById(long id) {
+        String hql = "FROM Meeting as meeting where meeting.id =" + id;
+        Query query = connector.getSession().createQuery(hql);
+        return (Meeting) query.uniqueResult();
+    }
 
-
+    public void createMeeting(Meeting meeting) {
+        Transaction transaction = connector.getSession().beginTransaction();
+        connector.getSession().save(meeting);
+        transaction.commit();
+    }
 }
